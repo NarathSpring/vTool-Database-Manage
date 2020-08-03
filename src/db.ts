797d659c64399305aa6fa987b43145b3f60f09c4
valentine <@yahoo.com>
2020-08-03 18:52:05 +0800

    建表时默认设定utf8字符集

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,15 @@
 import mysql from "mysql";
 
 class VTool {
-  private connection: mysql.Connection;
+  private _connection: mysql.Connection;
+  private _table: string = "";
+  private _database: string = "";
+
+  // 待插入的内容
+  public content = {};
 
   constructor(public config: mysql.ConnectionConfig) {
-    this.connection = mysql.createConnection({
+    this._connection = mysql.createConnection({
       host: this.config.host,
       port: this.config.port,
       user: this.config.user,
@@ -14,7 +19,7 @@ class VTool {
   }
 
   connect() {
-    this.connection.connect((err) => {
+    this._connection.connect((err) => {
       if (err) {
         console.log(err);
         return;
@@ -24,7 +29,7 @@ class VTool {
   }
 
   end() {
-    this.connection.end((err) => {
+    this._connection.end((err) => {
       if (err) {
         console.log(err);
         return;
@@ -37,7 +42,7 @@ class VTool {
   show(database_name: string, table_name?: string) {
     if (arguments.length === 1) {
       const q = "SHOW DATABASES";
-      this.connection.query(q, (err, results, fields) => {
+      this._connection.query(q, (err, results, fields) => {
         if (err) {
           console.log(err);
           return;
@@ -49,61 +54,63 @@ class VTool {
     }
   }
 
-  add() {}
+  save(object: object, table_name?: string) {
+    // let arr: string[] = []
+    // for (var i in object) {
+    //   if (object.hasOwnProperty(i)) {
+    //     arr.push(i)
+    //   }
+    // }
+    console.log(object);
+
+    const q = `INSERT INTO ${table_name} SET ?`;
+    this._connection.query(q, object, (err, results, fields) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(results);
+    });
+  }
 
   remove() {}
 
   delete() {}
 
   useDatabase(database_name: string) {
-    this.connection.config.database = database_name;
-    console.log(this.connection.config.database);
-  }
-  createDatabase(database_name: string) {
-    // CREATE DATABASE IF NOT EXISTS dabase_name
-    const q = `CREATE DATABASE ${database_name}`;
-    this.connection.query(q, (err, results, fields) => {
-      if (err) {
-        switch (err.errno) {
-          case 1007:
-            console.log("数据库已经存在");
-            break;
-          default:
-            console.log(err.code + ": " + err.errno);
-            break;
-        }
-        return;
-      }
-      console.log("创建数据库成功：");
-      // console.log(results);
-    });
-  }
-
-  dropDatabase(database_name: string) {
-    // DROP DATABASE database_name
+    this._connection.config.database = database_name;
+    this._database = database_name;
+    // console.log(this._connection.config.database);
   }
 
   createTable(table_name: string, columns: string[]) {
     //  CREATE TABLE table_name (content)
-    // const q = `CREATE TABLE IF NOT EXISTS ${content}`
     let s: string[] = [];
     columns.map((i) => {
       s.push(i + " VARCHAR(255)");
     });
-    const q = `CREATE TABLE IF NOT EXISTS ${table_name} (userId INT AUTO_INCREMENT PRIMARY KEY, ${s})`;
+    const q = `CREATE TABLE IF NOT EXISTS ${table_name} (userId INT AUTO_INCREMENT PRIMARY KEY, ${s}) CHARSET=utf8`;
     console.log(q);
 
-    // this.connection.query(q, (err, results, fields) => {
-    //   if (err) {
-    //     console.log(err);
-    //     return;
-    //   }
-    //   console.log(results);
-    // });
+    this._connection.query(q, (err, results, fields) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(results);
+    });
   }
 
   dropTable(table_name: string) {
     // DROP TABLE table_name
+    const q = `DROP TABLE ${table_name}`;
+    this._connection.query(q, (err, results, fields) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(results);
+    });
   }
 
   trncateTable(table_name: string) {
